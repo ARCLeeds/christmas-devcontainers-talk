98535850a28a9c5fcf1e20989cdeeb712a5298e5
Sparrow0hawk <alexjcoleman@gmail.com>
2022-12-02 21:01:58 +0000

    updated random_walk
    specifies limits
    uses vstack to combine generated arrays
    plots santa's location and walking points correctly

--- src/random_walk.py
+++ src/random_walk.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    santa_warehouse = np.zeros([100,100])
+    limits = [1000,1000]
 
-    santa = (np.random.randint(100), np.random.randint(100))
+    santa_warehouse = np.zeros(limits)
+
+    santa = (np.random.randint(limits[0]), np.random.randint(limits[0]))
 
     start = np.asarray([0,0])
     x = []
@@ -14,23 +16,25 @@ def main():
     for i in range(n):
         step = np.asarray([random_step(), random_step()])
         start = start + step 
-        while any(start < 0 ) or any(start > 100):
+        while any(start < 0 ) or any(start > limits[0]):
             step = np.asarray([random_step(), random_step()])
             start = start + step 
         x.append(start)
 
-    plot_walk(x)
+    x = np.vstack(x)
+    plot_walk(x, santa)
 
     return 0
 
 def random_step(prob=0.5):
     return np.random.choice([-1,1], p=[prob,prob])
 
-def plot_walk(data: list):
+def plot_walk(data: list, santa: tuple):
 
-    plt.plot(data)
-    plt.xlabel("Steps", fontsize=20)
-    plt.ylabel(r"$S_{n}$")
+    plt.plot(data[:,0], data[:,1])
+    plt.plot(santa[0], santa[1], marker="x", color="red")
+    plt.xlabel("lon", fontsize=20)
+    plt.ylabel("lat", fontsize=20)
     plt.title("Santa's elves walks")
 
     plt.savefig("figures/random_walks.png", dpi=300)
