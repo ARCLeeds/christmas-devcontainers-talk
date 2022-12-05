b53d564b7763d8ae5a03f683e88d5dcf14112f3e
Sparrow0hawk <alexjcoleman@gmail.com>
2022-12-05 09:01:32 +0000

    add mpi logic for each markov chain

--- src/random_walk.py
+++ src/random_walk.py
@@ -1,37 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpi4py import MPI
+
 
 def main():
 
-    limits = [1000,1000]
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
 
-    santa_warehouse = np.zeros(limits)
-
-    santa = (np.random.randint(limits[0]), np.random.randint(limits[0]))
-
-    start = np.asarray([0,0])
     x = []
-    n = 10_000
+    n = 1000
 
+    if rank == 0:
+
+        limits = [100, 100]
+
+        santa_warehouse = np.zeros(limits)
+
+        santa = (np.random.randint(limits[0]), np.random.randint(limits[0]))
+
+        start = np.asarray([0, 0])
+
+    else:
+        limits, santa_warehouse, santa = (
+            None,
+            None,
+            (np.empty(1, dtype="i"), np.empty(1, dtype="i")),
+        )
+        start = np.empty([1, 1], dtype="i")
+
+    limits = comm.bcast(limits, root=0)
+    santa_warehouse = comm.bcast(santa_warehouse, root=0)
+    santa = comm.bcast(santa, root=0)
+    start = comm.bcast(start, root=0)
+
+    print("Running markov chain on rank: ", rank)
     for i in range(n):
         step = np.asarray([random_step(), random_step()])
-        start = start + step 
-        while any(start < 0 ) or any(start > limits[0]):
-            step = np.asarray([random_step(), random_step()])
-            start = start + step 
+        start = start + step
+        if any(start < 0):
+            start[start < 0] = random_step(1.0)
+        elif any(start > limits[0]):
+            start[start > limits[0]] = random_step(0)
+        else:
+            start
         x.append(start)
 
-    x = np.vstack(x)
-    plot_walk(x, santa)
+    x = list(x)
+
+    data = comm.gather(x, root=0)
+
+    if rank == 0:
+        plot_walk(data, santa)
 
     return 0
 
+
 def random_step(prob=0.5):
-    return np.random.choice([-1,1], p=[prob,prob])
+    return np.random.choice([-1, 1], p=[1 - prob, prob])
+
 
 def plot_walk(data: list, santa: tuple):
 
-    plt.plot(data[:,0], data[:,1])
+    for item in data:
+        item = np.vstack(item)
+        plt.plot(item[:, 0], item[:, 1])
     plt.plot(santa[0], santa[1], marker="x", color="red")
     plt.xlabel("lon", fontsize=20)
     plt.ylabel("lat", fontsize=20)
